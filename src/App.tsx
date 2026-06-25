8725642c830f8c554242d4d41bb973ec1cfe2fb8
ُEzzdin Atef <ezzdin1125@gmail.com>
2026-06-25 15:27:06 +0300

    refactor: remove unused App.css and clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import { invoke } from "@tauri-apps/api/core";
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <main className="container">
+    <main className="">
       <h1>Welcome to Tauri + React</h1>
     </main>
   );
